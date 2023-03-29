b1c5d3ff486c6c273d8050bbcef35786d2f45a47
PavlovichVladislav <jeffry2814@gmail.com>
2023-03-29 16:18:08 +0700

    add routers, endpoints and controllers

---
+++ server/routes/brandRouter.js
@@ -0,0 +1,9 @@
+const Router = require('express');
+
+const router = new Router();
+
+router.post('/',)
+router.get('/',)
+// можно ещё delete
+
+module.exports = router;
---
+++ server/routes/deviceRouter.js
@@ -0,0 +1,10 @@
+const Router = require('express');
+
+const router = new Router();
+
+router.post('/',)
+router.get('/',)
+router.get('/:id',)
+// можно ещё delete
+
+module.exports = router;
