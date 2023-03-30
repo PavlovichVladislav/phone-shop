e22080a68170d61de7167b7602b82fd493476df8
PavlovichVladislav <jeffry2814@gmail.com>
2023-03-30 16:24:28 +0700

    add api error handler

--- server/index.js
+++ server/index.js
@@ -2,10 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
 const cors = require('cors');
-
 const models = require('./models/models');
-
 const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,6 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());  
 app.use('/api', router);
+
+app.use(errorHandler);
 
 const start = async () => {
   try {
